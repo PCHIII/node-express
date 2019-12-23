df61ca0362e4b114c984e9445194edfcdd3c7750
PCHIII <treyhelmer@gmail.com>
2019-12-22 22:40:29 -0500

    initial upload

---
+++ todo.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const router = express.Router()
+
+const todosByUser = {}
+
+router.post('/', (req, res, next) => {
+  const todo = [...req.body.todo || []]
+  if (req.body.remove) todo.splice(req.body.remove, 1)
+  if (req.body.new) todo.push({})
+
+  todosByUser[req.userContext.userinfo.sub] = todo
+
+  next()
+})
+
+router.use('/', (req, res) => {
+  const todo = todosByUser[req.userContext.userinfo.sub] || []
+
+  res.render('todo', { 
+    title: ' { To-Do List } ', 
+    todo, 
+    userinfo: req.userContext.userinfo,
+    style: 'home.css'
+   })
+})
+
+module.exports = router
